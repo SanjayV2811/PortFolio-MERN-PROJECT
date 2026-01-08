f86ebd7cd0af6733fef7066b371f571819095ff1
Sanjay2811-V <diliprbaviskar77@gmail.com>
2026-01-08 21:49:48 +0530

    New cors added

--- Backend/app.js
+++ Backend/app.js
@@ -20,7 +20,8 @@ app.use("/uploads",express.static("uploads"));
 app.use(express.static(path.join(__dirname,"public")));
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://portfolio1128sanjay.netlify.app/"],
+  
   
   credentials: true
 }))
